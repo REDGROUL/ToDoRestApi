c8df306424ebf9e3570247cde1435a146bac4d2d
redgroul <redgroulman@gmail.com>
2024-02-27 20:38:47 +0300

    методы: добавления, удаления и обновления таски

--- src/controller/TaskController.php
+++ src/controller/TaskController.php
@@ -14,5 +14,30 @@ class TaskController {
         return json_encode($this->taskModel->findAllTasks());
     }
 
+    public function dropTaskById($id) {
+        try{
+            return json_encode($this->taskModel->dropTaskById($id));
+        } catch(\Exception $exp) {
+            return json_encode(["error"=>"not found"]);
+        }
+    }
+
+    public function addTask($array) {
+        try{
+            return json_encode($this->taskModel->addTask($array));
+        } catch(\Exception $exp) {
+            return json_encode(["error"=>"can't save"]);
+        }
+    }
+
+    public function taskDone($id) {
+        try{
+            return json_encode($this->taskModel->taskDone($id));
+        } catch(\Exception $exp) {
+            return json_encode(["error"=>"not found"]);
+        }
+    }
+  
+
 
 }
